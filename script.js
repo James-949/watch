d73328b2cf22c32d891c05a5d2daf9650c46e4ed
James-949 <130909119+James-949@users.noreply.github.com>
2023-04-16 06:02:00 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
-﻿var th = 0;
+var th = 0;
 var dq = 0;
 var sa = 0;
 var i = 0;
 while (i < 1) {
     i++;
-    if (th = 567) {
-        th = 0;
-        dq = 1;
-    }
-        if (dq = 30) {
-            dq = 0;
-            sa = 1;
-        }
-            if (sa = 7) {
-                sa = 0;
-            }
     document.getElementById("sa").innerHTML = sa;
     document.getElementById("dq").innerHTML = dq;
     document.getElementById("th").innerHTML = th;
+    if (th = 576) {
+        th = 0;
+        dq = 1;
+    } else if (dq = 30) {
+        dq = 0;
+        sa = 1;
+    } else if (sa = 5) {
+        sa = 0;
+    }
 }
